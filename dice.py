c3f159ecf3021069f7e19362514bdfc8d6c1ca75
Oxshy <88091831+Oxshy@users.noreply.github.com>
2022-09-03 11:50:44 +0530

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,11 @@
+import random
+rolldie=input("do you want to roll the dice ")
+while rolldie=="yes":
+    print(random.randint(1,6))
+    print(random.randint(1,6))
+    rolldie=input("do you want to roll again? ")
+
+
+
+
+
